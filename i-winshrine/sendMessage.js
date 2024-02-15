835ac02faac6e01303e1dd379c4c90e7040b6bcd
i-fail24 <126712774+ifail2x@users.noreply.github.com>
2024-02-15 18:39:26 +0000

    Add files via upload

---
+++ i-winshrine/sendMessage.js
@@ -0,0 +1,6 @@
+function sendMessage() {
+      const request = new XMLHttpRequest();
+      request.open("POST", "https://discordapp.com/api/webhooks/676118118082281513/ZS5YcWhurzokBrKX9NgexqtxrJA5Pu2Bo4i7_JsIxC-JIbPBVhSZkcVVukGOro52rnQA");
+      // replace the url in the "open" method with yours
+      request.setRequestHeader('Content-type', 'application/json');
+}
